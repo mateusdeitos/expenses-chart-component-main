cae6a82ea7ea95f24842112e935b416d60685513
Mateus Deitos <mate.deitos@hotmail.com>
2023-04-22 08:57:24 -0300

    fix: center content and gh-pages to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+	base: "/frontend-mentor-expenses-chart-component/",
+	plugins: [svelte()],
 })
